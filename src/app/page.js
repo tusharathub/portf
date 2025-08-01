48a8226cd130821ae7f7fcf1aa96df9a97fdb21f
Tushar <tushar.nyc20@gmail.com>
2025-08-01 16:52:15 +0530

    starting

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function App() {
   return(
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
      
-      <SplashCursor/>
+      {/* <SplashCursor/> */}
       <HomePage/>
       
     </div>
