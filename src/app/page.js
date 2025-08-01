7e346ee0d94dcacf03d466725f4b0195e893b3ee
Tushar <tushar.nyc20@gmail.com>
2025-08-01 15:45:22 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import SplashCursor from '../../bits/SplashCursor/SplashCursor';
+import HomePage from './components/homepage'
+export default function App() {
+  return(
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+     
+      <SplashCursor/>
+      <HomePage/>
+      
+    </div>
+  )
+}
